e4709fd8e1d123ad89516cc88c3c75d585f4b060
sharan <kaursharan1205@gmail.com>
2026-06-23 18:03:24 +0530

    Initial commit

--- lambda.py
+++
@@ -1,3 +0,0 @@
-square=lambda x:x**2
-num=int(input("enter a number:"))
-print("suare of",num,"is",square(num))
--- tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-
-    ("100236584739", "Neha", "Kaur", "Savings account", "9123456789", "Female", "15-06-2002", "neha@outlook.com", "3500")
